fcc35e78d8ca593c12e90c7e0cfb14257a2ccf98
Jens Møller <jens@jensz12.com>
2016-05-17 21:07:00 +0200

    Create video-list.php

---
+++ youtube/video-list.php
@@ -0,0 +1,17 @@
+<?php
+$videos = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?key=API-KEY&part=snippet&channelId=CHANNEL-ID&order=date&maxResults=10'), true);
+$latest_video_id = $videos['items'][0]['id']['videoId'];
+?>
+
+<?php foreach ($videos['items'] as $video): ?>
+				<div class="col-sm-6">
+					<section class="thumbnail">
+						<a href="https://youtu.be/<?php echo $video['id']['videoId']; ?>">
+							<img class="img-responsive" src="https://i.ytimg.com/vi/<?php echo $video['id']['videoId']; ?>/maxresdefault.jpg" />
+						</a>
+						<div class="caption">
+							<a href="https://youtu.be/<?php echo $video['id']['videoId']; ?>"><h4 class="text-ellipsis"><?php echo $video['snippet']['title'];?></h4></a>
+						</div>
+					</section>
+				</div>
+				<?php endforeach; ?>
